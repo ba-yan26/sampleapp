4db3a7756931fb3cdc04bb1d26e6ec0e96ebc1a8
EC2 Default User <ec2-user@ip-172-31-30-202.ec2.internal>
2022-01-07 01:01:43 +0000

    [Upload]4-4,詳細画面の作成,名前付きルート作成

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -9,7 +9,7 @@ class ListsController < ApplicationController
     # データを受け取り新規登録するためのインスタンス作成(ビューファイルへの引き渡しが必要ないのでローカル変数)
     list.save
     # データをデータベースに保存するためのsaveメソッド実行
-    redirect_to '/top'
+    redirect_to list_path(list.id)
     
   end
 
@@ -18,6 +18,7 @@ class ListsController < ApplicationController
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
